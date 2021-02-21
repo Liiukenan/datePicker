62a29c65b57767f07cffe8fd563aa8f58b5e3618
Liiukenan <305290526@qq.com>
2021-02-21 22:56:23 +0800

    修改接口

--- src/api/serverPath.js
+++ src/api/serverPath.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 if(process.env.CMLINT_ENV === 'production'){
-    axios.defaults.baseURL='http://54.223.205.242:8300/'
+    axios.defaults.baseURL='https://www.fastmock.site/mock/9dfa740c2dcc093fde885622066768ec/datedata/'
 }else{
-    axios.defaults.baseURL='http://54.223.205.242:8300/'
+    axios.defaults.baseURL='https://www.fastmock.site/mock/9dfa740c2dcc093fde885622066768ec/datedata/'
 }
 export function _getDateData(data){
-    return axios.post('v1/anchor/income-report-dates',data)
+    return axios.post('date',data)
 }
 export function _getData(data){
     return axios.post('v1/anchor/income-report-detail',data)
